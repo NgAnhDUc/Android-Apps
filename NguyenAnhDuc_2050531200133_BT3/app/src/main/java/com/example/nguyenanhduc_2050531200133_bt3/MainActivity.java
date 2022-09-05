408f12fed9a091e8c710a8c6311f4647853f3ec0
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2022-09-05 18:16:24 +0700

    Chuyenactivity

---
+++ NguyenAnhDuc_2050531200133_BT3/app/src/main/java/com/example/nguyenanhduc_2050531200133_bt3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nguyenanhduc_2050531200133_bt3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnOpen=(Button)
+                findViewById(R.id.btnOpenChildActivity);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                doOpenChildActivity();
+            }
+        });
+    }
+    public void doOpenChildActivity()
+    {
+        Intent myIntent=new Intent(this, ChildActivity.class);
+        startActivity(myIntent);
+    }
+}
